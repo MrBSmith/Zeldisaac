892063b38454d86da0ed8ab4765c115027a0f581
Hugo Baba <Hugobarette@gmail.com>
2021-10-19 14:46:25 +0200

    Character move animations

--- Scenes/Character/Character.gd
+++ Scenes/Character/Character.gd
@@ -1,8 +1,9 @@
 extends KinematicBody2D
 
+onready var animated_sprite = get_node("AnimatedSprite")
+
 var speed = 300.0
 var direction = Vector2.ZERO
-
 
 func _process(_delta: float) -> void:
 	move_and_slide(direction * speed)
@@ -11,5 +12,18 @@ func _process(_delta: float) -> void:
 func _input(_event: InputEvent) -> void:
 	direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	
+	direction = direction.normalized()
+	
+	match(direction):
+		Vector2.RIGHT: animated_sprite.play("MoveRight")
+		Vector2.LEFT: animated_sprite.play("MoveLeft")
+		Vector2.DOWN: animated_sprite.play("MoveDown")
+		Vector2.UP: animated_sprite.play("MoveUp")
+		Vector2.ZERO:
+			animated_sprite.stop()
+			animated_sprite.set_frame(0)
+
+
 
 
