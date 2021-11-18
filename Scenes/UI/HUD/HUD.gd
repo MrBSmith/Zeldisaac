4d4088b2e1c889ebd6270fd6aac4bb7020b9a25f
Hugo Baba <Hugobarette@gmail.com>
2021-11-18 11:47:27 +0100

    Prepare for rebase

--- Scenes/UI/HUD/HUD.gd
+++ Scenes/UI/HUD/HUD.gd
@@ -1,13 +1,12 @@
 extends TextureRect
 
-onready var coin_counter_label = $CoinCounter
+onready var coin_counter = $CoinCounter
 
 
 func _ready() -> void:
-	EVENTS.connect("nb_coins_changed", self, "_on_EVENTS_nb_coins_changed")
-
+	var __ = EVENTS.connect("nb_coins_changed", self, "_on_EVENTS_nb_coins_changed")
 
 
 
 func _on_EVENTS_nb_coins_changed(nb_coins: int) -> void:
-	coin_counter_label.set_text(String(nb_coins))
+	coin_counter.set_text(String(nb_coins))
